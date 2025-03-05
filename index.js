0a370bd71a5e72b78305a4bda005f30bbaf0f1b5
Pranshoo <pranshoo@gmail.com>
2025-03-05 18:50:14 +0530

    added security code

---
+++ index.js
@@ -0,0 +1,88 @@
+require('dotenv').config()
+const express = require('express');
+const path = require('node:path');
+const https = require('node:https');
+const fs = require('node:fs');
+const helmet= require('helmet');
+const passport = require('passport');
+const cookieSession = require('cookie-session');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+
+const app = express();
+
+const config={
+    clientID: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    callbackURL: process.env.CALLBACK_URL,
+    cookieKey: process.env.COOKIE_KEY
+}
+
+app.use(cookieSession({
+    name: 'session',
+    maxAge: 1000*60*60*24,
+    keys: [config.cookieKey, 'dsadfasdas'],
+    resave: false,
+    saveUninitialized: true,
+}));
+
+app.use(function (request, response, next) {
+    if (request.session && !request.session.regenerate) {
+      request.session.regenerate = (cb) => {
+        cb();
+      };
+    }
+    if (request.session && !request.session.save) {
+      request.session.save = (cb) => {
+        cb();
+      };
+    }
+    next();
+  });
+
+const verifyCallback = (accessToken, refreshToken, profile, done)=>{
+    console.log('Google profile', profile)
+    done(null, profile);
+}
+
+passport.use(new GoogleStrategy(config, verifyCallback));
+
+// to save the user data into cookie
+passport.serializeUser((user, done)=>{
+    console.log(user)
+    done(null, user.id)
+});
+
+// to read the data from the cookie
+passport.deserializeUser((obj, done)=>{
+    done(null, obj)
+});
+
+app.use(helmet());
+
+app.use(passport.initialize());
+
+app.use(passport.session());
+
+app.get('/auth/google', passport.authenticate('google', {scope: ['profile']}));
+
+app.get('/auth/google/callback', passport.authenticate('google', {
+    failureRedirect: '/',
+    successRedirect: '/secret'
+}));
+
+app.get('/auth/logout', (req, res)=>{});
+
+app.get('/secret', (req, res)=>{
+    res.send('Your secret value is 9100');
+});
+
+app.get('/', (req, res)=>{
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+https.createServer({
+    key: fs.readFileSync('key.pem'),
+    cert: fs.readFileSync('cert.pem')
+}, app).listen(3000, ()=>{
+    console.log('Server started at port 3000')
+});
